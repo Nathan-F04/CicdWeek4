c1c7ea9bc821cab60f6916e93bbe01b24a720332
G00413711@atu.ie <G00413711@atu.ie>
2024-10-09 15:01:05 +0100

    added annotation to person class for constructors as well as variables with annotation

--- src/main/java/ie/atu/week4cicd/Person.java
+++ src/main/java/ie/atu/week4cicd/Person.java
@@ -1,4 +1,31 @@
 package ie.atu.week4cicd;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
+
+    @NotBlank(message="You must enter a name")
+    private String name;
+    @NotBlank(message="You must enter a title")
+    private String title;
+    @PositiveOrZero(message="You must enter a valid id")
+    private int employeeId;
+    @PositiveOrZero(message="You must enter a valid age")
+    private int age;
+    @Email(message="You must enter a valid email")
+    private String email;
+    @NotBlank(message="You must enter a position")
+    private String position;
+    @NotBlank(message="You must enter a department")
+    private String department;
+
 }
